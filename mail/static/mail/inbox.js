873f8e63c9d8e5a655015de3f3c519d618011eaa
Tetiana Shelest <tashelest@gmail.com>
2023-06-23 13:00:46 +0200

    Fix bugs, add comments

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -20,7 +20,8 @@ function compose_email() {
   // Show compose view and hide other views
   let form = document.querySelector("#compose-view")
   form.style.display = "block"
-  document.querySelector("#mailbox-view").style.display = "none"
+  document.getElementById("mailbox-view").style.display = "none"
+  document.getElementById("single-email-view").style.display = "none"
 
   form.querySelector("h3").textContent = "New Email"
 
@@ -31,8 +32,10 @@ function compose_email() {
   form.querySelector("#compose-body").value = ""
 }
 
-function send_email(e) {
-  e.preventDefault()
+function send_email(event) {
+  // Prevent page refresh
+  event.preventDefault()
+  // Grab required values from the compose form
   let recipients = document.querySelector("#compose-recipients").value
   let subject = document.querySelector("#compose-subject").value
   let body = document.querySelector("#compose-body").value
@@ -45,24 +48,27 @@ function send_email(e) {
       body,
     }),
   })
-    .then((response) => response.json())
-    .then((result) => {
+    .then(response => response.json())
+    .then(result => {
       if ("error" in result) {
         alert(result.error)
       } else {
         load_mailbox("sent")
       }
     })
+    .catch(error => {
+      alert("Something went wrong")
+      console.error(error.message)
+    })
 }
 
 function load_mailbox(mailbox) {
   let mailboxView = document.querySelector("#mailbox-view")
-  mailboxView.style.display = "block"
-  // // Show the mailbox name
+  // Show the mailbox name
   mailboxView.querySelector("h3").innerText = `${
     mailbox.charAt(0).toUpperCase() + mailbox.slice(1)
   }`
-  // Show the mailbox and hide other views
+  // Hide other views
   document.querySelector("#compose-view").style.display = "none"
   document.querySelector("#single-email-view").style.display = "none"
   document.querySelector("#empty-mailbox-alert").style.display = "none"
@@ -70,15 +76,15 @@ function load_mailbox(mailbox) {
 
   // Update the mailbox with the latest emails to show for this mailbox
   fetch(`/emails/${mailbox}`)
-    .then((response) => response.json())
-    .then((emails) => {
+    .then(response => response.json())
+    .then(emails => {
       if (emails.length == 0) {
         document.querySelector("#empty-mailbox-alert").style.display = "block"
       } else {
         document.querySelector("#mailbox-content").style.display = "table"
         let listContent = mailboxView.querySelector("tbody")
         listContent.innerHTML = ""
-        emails.forEach((email) => {
+        emails.forEach(email => {
           let row = document.createElement("tr")
           if (!email.read) {
             row.classList.add("unread")
@@ -89,33 +95,37 @@ function load_mailbox(mailbox) {
 
           listContent.append(row)
 
-          row.addEventListener("click", (e) => {
+          row.addEventListener("click", e => {
             view_email(email, mailbox)
           })
         })
       }
     })
+
+  // Show the mailbox
+  mailboxView.style.display = "block"
 }
 
 function view_email(email, mailbox) {
+  // Hide other views
   document.getElementById("compose-view").style.display = "none"
   document.getElementById("mailbox-view").style.display = "none"
 
+  // Show single email view
   let emailView = document.getElementById("single-email-view")
   emailView.style.display = "block"
   emailView.querySelector("h3").textContent = `${
     mailbox.charAt(0).toUpperCase() + mailbox.slice(1)
   }`
 
-  let archiveButton = document.getElementById("archive-button")
+  // Manage visibility of Archive button depending on the specific mailbox
+  let archiveButton = document.querySelector("#archive-button")
+  archiveButton.style.display = mailbox === "sent" ? "none" : "block"
 
-  if (mailbox === "sent") {
-    archiveButton.style.display = "none"
-  }
-
+  // Populate email view fields with the required content
   fetch(`/emails/${email.id}`)
-    .then((response) => response.json())
-    .then((email) => {
+    .then(response => response.json())
+    .then(email => {
       document.querySelector("#email-subject span").textContent = email.subject
       document.querySelector("#email-sender span").textContent = email.sender
       document.querySelector("#email-recepients span").textContent =
@@ -127,38 +137,44 @@ function view_email(email, mailbox) {
         email.body
     })
 
-  fetch(`/emails/${email.id}`, {
-    method: "PUT",
-    body: JSON.stringify({
-      read: true,
-    }),
-  })
+  // Mark email as read
+  if (!email.read) {
+    fetch(`/emails/${email.id}`, {
+      method: "PUT",
+      body: JSON.stringify({
+        read: true,
+      }),
+    })
+  }
 
   let replyButton = document.getElementById("reply-button")
   replyButton.addEventListener("click", () => reply(email))
 
-  // archiveButton.textContent = mailbox === "archive" ? "Unarchive" : "Archive"
-  archiveButton.textContent = email.archived ? "Unarchive" : "Archive"
+  // Change the text of Archive button depending on the specific mailbox
+  archiveButton.textContent = mailbox === "archive" ? "Unarchive" : "Archive"
 
-  archiveButton.addEventListener("click", () =>
+  archiveButton.onclick = () => {
     archive(email.id, email.archived)
-  )
+  }
 }
 
 function reply(email) {
+  // Hide email view and show compose form
   document.getElementById("single-email-view").style.display = "none"
-
   let form = document.querySelector("#compose-view")
   form.style.display = "block"
+  // Change the form title
   form.querySelector("h3").textContent = "Reply"
 
   fetch(`/emails/${email.id}`)
-    .then((response) => response.json())
-    .then((email) => {
+    .then(response => response.json())
+    .then(email => {
+      // Pre-fill fields with the email details
       document.querySelector("#compose-recipients").value = email.sender
       document.querySelector("#compose-subject").value =
         email.subject.startsWith("Re:") ? email.subject : `Re: ${email.subject}`
       document.querySelector("#compose-recipients").disabled = true
+      // Pre-fill body field with a line like "On Jan 1 2020, 12:00 AM foo@example.com wrote:"
       document.querySelector("#compose-body").value =
         `\n\n ...\nOn ${email.timestamp} ${email.sender} wrote:\n` +
         email.body.replace(/^/gm, "  ")
@@ -171,8 +187,5 @@ function archive(id, archived) {
     body: JSON.stringify({
       archived: !archived,
     }),
-  }).then((response) => console.log(response))
-
-  // location.reload()
-  load_mailbox("inbox")
+  }).then(() => load_mailbox("inbox"))
 }
